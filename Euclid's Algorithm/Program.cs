c1aaddf8be0a8cb229de977f3c66d4ab51baf2a1
MayaPC <MayaPC@MAYA>
2022-10-26 15:53:45 +0200

    Add project files.

---
+++ Euclid's Algorithm/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Euclid
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Pick num1");
+            string num1S = Console.ReadLine();
+            Console.WriteLine("Pick num2");
+            string num2S = Console.ReadLine();
+
+            float num1 = float.Parse(num1S);
+            float num2 = float.Parse(num2S);
+
+            while (num1 != num2)
+            {
+                if(num1 > num2)
+                {
+                    num1 = num1 - num2;
+                    Console.WriteLine(num1);
+                }
+                else
+                {
+                    num2 = num2 - num1;
+                    Console.WriteLine(num2);
+                }
+            }
+
+            Console.WriteLine("The GCD of your numbers is: " + num1);
+        }
+    }
+}
